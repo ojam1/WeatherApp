1c23cd29e5e6ff32132fc385f6282c15d4e64ccb
ojam1 <oranson1@gmail.com>
2023-07-09 14:08:55 +0100

    add navgation

---
+++ components/screens/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+import appStyles from '../../../appStyles';
+
+export default () => (
+  <View style={appStyles.main}>
+    <Text>Home</Text>
+  </View>
+);
